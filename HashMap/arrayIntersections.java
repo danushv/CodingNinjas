3e9df35b5d1369f3b4fbf4ca2fe91e0276513f11
danny boi <43886538+danushv@users.noreply.github.com>
2023-12-09 19:17:35 -0600

    Update and rename arrayIntersections to arrayIntersections.java

--- HashMap/arrayIntersections.java
+++ HashMap/arrayIntersections.java
@@ -6,12 +6,12 @@ public class Solution {
 	public static void printIntersection(int[] arr1,int[] arr2){
 		/* Your class should be named Solution
 		 * Don't write main().
-		 * Don't read input, it is passed as function argument.
+		 * Don't read input, it is passed as a function argument.
 		 * Return output and don't print it.
 	 	 * Taking input and printing output is handled automatically.
         */
 		// Algorithm
-		// you put one array in hashmap and another keep it in array because ypu also want duplicate elements 
+		//You put one array in hashmap and another, keep it in the array because ypu also want duplicate elements 
 		// in case if you dont want duplicate elements use two hashmap and you can also create for each loops 
 		// go thru the for loop of arr and check if it is present in hashmap and print
 		// why you use hashmap -> it saves up some time because of faster lookup
