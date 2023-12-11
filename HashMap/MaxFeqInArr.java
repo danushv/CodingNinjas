db44eb1e3c612f18f9999b3b86cdef81fb2c618e
danny boi <43886538+danushv@users.noreply.github.com>
2023-12-10 23:04:51 -0600

    Update MaxFeqInArr.java

--- HashMap/MaxFeqInArr.java
+++ HashMap/MaxFeqInArr.java
@@ -7,7 +7,7 @@ public class Solution {
 		 * Don't write main().
 		 * Don't read input, it is passed as function argument.
 		 * Return output and don't print it.
-	 	 * Taking input and printing output is handled automatically.
+	 	 * Taking input and printing output is thadled automatically.
 		*/
 		HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
 		for(int i=0;i<arr.length;i++)
