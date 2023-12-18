7c4af095c9ebca2f292ccbaa40de7a3d7dec2840
danny boi <43886538+danushv@users.noreply.github.com>
2023-12-18 12:12:43 -0600

    own

---
+++ Recursion2/replaceCharacter.java
@@ -0,0 +1,31 @@
+
+public class Solution {
+
+	public static String replaceCharacter(String input, char c1, char c2) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+
+
+		if(input.length()<=0)
+		{
+			return " ";
+		}
+
+		if(input.charAt(0)==c1)
+		{
+					String smalloutput=replaceCharacter(input.substring(1),c1,c2);
+					return c2+smalloutput;
+		}
+		else
+		{
+				String smalloutput=replaceCharacter(input.substring(1),c1,c2);
+					return input.charAt(0)+smalloutput;
+		}
+
+		//return input;
+	}
+}
