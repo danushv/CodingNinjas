b2c6419410e6a59b456627594bda98692f6d4a66
danny boi <43886538+danushv@users.noreply.github.com>
2023-12-25 17:52:23 -0600

    Create DynamicProgramming.java

---
+++ DynamicProgramming.java
@@ -0,0 +1,42 @@
+
+
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+
+        int m=grid.length;
+        int n=grid[0].length;
+
+        int[][] dp=new int [m][n];
+
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+//last element
+                if(i==m-1&&j==n-1)
+                {
+                    dp[i][j]=grid[i][j];
+                }
+  // last row
+                else if(i==m-1)
+                {
+                    dp[i][j]=dp[i][j+1] +grid[i][j];
+                }
+// last column
+                else if(j==n-1)
+                {
+                    dp[i][j]=dp[i+1][j]+grid[i][j];
+                }
+// all other elements
+                else
+                {
+                    dp[i][j]=Math.min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+                }
+            }
+        }
+
+        return dp[0][0];
+        
+    }
+}
